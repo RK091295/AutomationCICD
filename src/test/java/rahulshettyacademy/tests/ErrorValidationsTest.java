a0ec2ac148dd7cca45d837dc8163abceacaff00a
Rohit R Kulkarni <rohit.rk12345@gmail.com>
2025-02-20 21:12:09 +0530

    Updated at 20-03-2025 2115

--- src/test/java/rahulshettyacademy/tests/ErrorValidationsTest.java
+++ src/test/java/rahulshettyacademy/tests/ErrorValidationsTest.java
@@ -23,7 +23,7 @@ public class ErrorValidationsTest extends BaseTest {
 
 	
 		landingPage.loginApplication("anshika@gmail.com", "Iamki000");
-		Assert.assertEquals("Incorrect email or passwore.", landingPage.getErrorMessage());
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
 
 	}
 	
